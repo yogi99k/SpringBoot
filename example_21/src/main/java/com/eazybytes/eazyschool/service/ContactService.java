7b679d5bdc0e34190b117bb982a9255637027418
yogi99k <yogikoteru99@gmail.com>
2026-01-17 11:54:27 -0500

    Example 18,20,21,22

---
+++ example_21/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -0,0 +1,30 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+public class ContactService {
+
+    private static Logger log  = LoggerFactory.getLogger(ContactService.class);
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+}
