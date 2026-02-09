9f85b7591a37b96a8825e34b5847c54d0a995814
yogi99k <yogikoteru99@gmail.com>
2026-02-09 18:54:02 -0500

    MySql Connection

---
+++ example_34/src/main/resources/eazyschool_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO eazyschool.holidays
+(day, reason, type, created_at, created_by)
+VALUES
+('Oct 31', 'Halloween', 'FESTIVAL', CURDATE(), 'DBA'),
+('Nov 24', 'Thanksgiving Day', 'FESTIVAL', CURDATE(), 'DBA'),
+('Dec 25', 'Christmas', 'FESTIVAL', CURDATE(), 'DBA'),
+('Jan 17', 'Martin Luther King Jr. Day', 'FEDERAL', CURDATE(), 'DBA'),
+('July 4', 'Independence Day', 'FEDERAL', CURDATE(), 'DBA'),
+('Sep 5', 'Labor Day', 'FEDERAL', CURDATE(), 'DBA'),
+('Nov 11', 'Veterans Day', 'FEDERAL', CURDATE(), 'DBA');
